2c6d2729230bbee0c82a9e1c1f36dabc57e88954
ctnohilly <ctnohilly@gmail.com>
2024-10-21 01:37:59 -0400

    Updating the port env reference

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
